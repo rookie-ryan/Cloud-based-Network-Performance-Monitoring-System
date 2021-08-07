c38fedf84d3908c8d3ee47dd6eecdfd62d452bc9
rookie-ryan <ryanlirui@outlook.com>
2021-08-07 20:25:58 +0800

    add gitignore

--- src/performance_test.py
+++ src/performance_test.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
 import json
-import socket
 import time
 import subprocess
+import sys
+import traceback
+from datetime import datetime
+import os
+from multiprocessing.dummy import Pool as ThreadPool
 
-vantage_file="../vantage_points.json"
-parameter_file="../monitor_parameters.json"
-output_path="../data/"
+# input and output path
+vantage_file= "vantage_points.json"
+parameter_file="monitor_parameters.json"
+output_path="data/"
 
 def get_monitor_ip(ip_list):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    local_ip = s.getsockname()[0]
-    s.close()
     with open(vantage_file, "r") as vantage_file_f:
         vantage_points=json.load(vantage_file_f)
         for point in vantage_points:
-            if vantage_points[point] != local_ip:
                 ip_list.append(vantage_points[point])
 
 def main():
@@ -29,17 +29,38 @@ def main():
         interval=parameters['interval']
         packet_size=parameters['size']
         packet_num=parameters['number']
+    t_p=ThreadPool()
     while True:
         for ip in ip_list:
-            cmd="sudo ping -c "+str(packet_num)+" -s "+str(packet_size)+" "+ip
-            p=subprocess.Popen(cmd, stdout=subprocess.PIPE, encoding='gbk')
-            output=p.communicate()[0]
-            print(output)
+            ping_cmd="sudo ping -c "+str(packet_num)+" -s "+str(packet_size)+" "+ip
+            # ping command will return 2 lines of summary results after "ping statistics"
+            grep_cmd = " | grep -A 2 statistics"
+            try:
+                current_month=datetime.now().strftime("%Y-%m")
+                current_day=datetime.now().strftime("%d")
+                current_time=datetime.now().strftime("%H:%M:%S")
+                output = subprocess.check_output(ping_cmd+grep_cmd,shell=True)
+                output=output.decode("utf-8")
+                loss_rate = output.split(",")[2]
+                rtt = output.split("rtt")[1]
+            except IndexError: # when split, as ping failed
+                # write fail infomation
+                a=1
+            except:
+                sys.stderr.write(traceback.format_exc())
+            else:
+                dir=output_path+current_month
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                write_data={}
+                write_data[current_time]={
+                    "loss rate":loss_rate,
+                    "rtt":rtt
+                }
+                output_file=dir+"/"+current_day+".json"
+                with open(output_file, "a+") as write_f:
+                    write_f.write(json.dumps(write_data) + "\n")
         time.sleep(interval)
-
-
-
-
 
 if __name__ == '__main__':
     main()
